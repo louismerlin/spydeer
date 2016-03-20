85838517955330e6d23f06b83bafdc84e978a8ee
louismerlin <louis.merlin@epfl.ch>
2016-03-20 22:11:36 +0100

    refix bug

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 class Spyder < Sinatra::Base
   get '/' do
-    @mac_address = Devices.all.map{|d| d.get(:mac_address)}
+    @mac_address = Device.all.map{|d| d.get(:mac_address)}
 
     erb :'public/index', :layout => :'public/layout'
 
